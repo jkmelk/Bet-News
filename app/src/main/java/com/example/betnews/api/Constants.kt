62170602fd15baebe35f4fc865cf2c554c4ac218
hovse <melkon@helix.am>
2020-11-25 18:56:18 +0400

    fixed api-key bug

--- app/src/main/java/com/example/betnews/api/Constants.kt
+++ app/src/main/java/com/example/betnews/api/Constants.kt
@@ -2,5 +2,5 @@ package com.example.betnews.api
 
 const val BASE_URL = "https://content.guardianapis.com/"
 const val SEARCH = "search"
-const val API_KEY = "test"
+const val API_KEY = "34a64e56-b306-4b9d-85d0-ed8961bbf5f1"
 const val DATABASE_NAME = "news database"
